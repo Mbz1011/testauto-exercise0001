28793c454bdb95289681497e128d3be50b6ab63f
John <jmikib@gmail.com>
2024-08-21 14:45:13 +0200

    some functionality

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import math
+
 def add(x, y):
     return x + y
 
@@ -10,19 +12,27 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+def exponent(x, y):
+    return x**y
+
+def squareroute(x):
+    return math.sqrt(x)
+
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5. Exponent Multiply")
+print("6. Square root of")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5/6): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4','5','6' ):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -41,6 +51,13 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+        
+        elif choice == '5':
+            print(num1, "^", num2, "=", exponent(num1, num2))
+
+        elif choice == '6':
+            num2 = 0
+            print("√", num1 ,"=", squareroute(num1))
         
         # check if user wants another calculation
         # break the while loop if answer is no
